a05f7afe070dc3df035f66ae57049a70db45fb11
纳兰天忆 <103520146@qq.com>
2017-01-11 14:29:40 +0800

    //

--- src/02/index.js
+++ src/02/index.js
@@ -6,10 +6,10 @@ import {render} from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from './containers/App';
-import todoApp from './reducers';
+import rootReducer from './reducers';
 
 
-let store = createStore(todoApp);
+let store = createStore(rootReducer);
 let rootElement = document.getElementById('root');
 render(
     <Provider store={store}>
